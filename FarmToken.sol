843eef92d01b3b5d2f75f6d913648837ccfb2821
Sirajul <161524666+sirajul9988@users.noreply.github.com>
2026-01-20 23:14:02 +0600

    Create FarmToken.sol

---
+++ FarmToken.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract FarmToken is ERC20, Ownable {
+    constructor() ERC20("Farm Reward Token", "FARM") Ownable(msg.sender) {
+        // Mint initial supply to deployer for testing
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
